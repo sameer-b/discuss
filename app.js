471ede7dcd046b1715ed0e88311cccc3d21d02d3
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2015-10-03 19:08:17 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var discuss = require('./lib/discuss.js');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * Routes start
+ */
+app.get('/getThread/:id', function (request, response) {
+    discuss.getPost(request.params.id, 'threads', request, response);
+});
+
+app.get('/getComments/:id', function (request, response) {
+    discuss.getPost(request.params.id, 'comments', request, response);
+});
+
+app.post('/newThread', function (request, response) {
+    discuss.newThread(request.body.author, request.body.title, request.body.body, request, response);
+});
+
+app.post('/newComment', function (request, response) {
+    discuss.newComment(request.body.id, request.body.author, request.body.title, request.body.body, request, response);
+});
+
+app.post('/getAllThreads', function (request, response) {
+    discuss.getAllPosts('threads', request, response);
+});
+/*
+ * Routes end
+ */
+
+/**
+ * Setup server
+ */
+var server = app.listen(3000, function () {
+  console.log('Starting Discuss! ');
+});
